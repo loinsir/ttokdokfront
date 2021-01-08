902aab6cd01dd98181f50d1a68b32deb9e68ba33
loinsir <a9327370@gmail.com>
2021-01-08 13:53:46 +0000

    app.js 리액트 라우터 돔 추가

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import './components/main/MainBookSlider';
 import MainBookSlider from './components/main/MainBookSlider';
+import { BrowserRouter, Route, Link, Switch} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img className="App-header-img" src="https://cdn.pixabay.com/photo/2017/09/11/15/34/animals-2739386_960_720.jpg" alt="logo"/>
-        <h1 id="mainTitle">똑똑?</h1>
-        <p className="App-intro">
-        </p>
-        <MainBookSlider></MainBookSlider>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <img className="App-header-img" src="https://cdn.pixabay.com/photo/2017/09/11/15/34/animals-2739386_960_720.jpg" alt="logo"/>
+          <Link to="/" className="anchor"><h1 id="mainTitle">똑똑?</h1></Link>
+          <Switch>
+            <Route exact path="/" component={MainBookSlider}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
