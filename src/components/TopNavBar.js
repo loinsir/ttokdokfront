8c699ee902090bb467c5698b3675b96d753b5ece
loinsir <a9327370@gmail.com>
2021-01-22 14:01:55 +0000

    네비게이션 바 추가

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Form, FormControl, Button } from "react-bootstrap";
 
 class TopNavBar extends Component {
     render() {
         return (
             <Navbar bg="dark" variant="dark" sticky="top">
                 <Navbar.Brand href="/"><h4>똑똑?</h4></Navbar.Brand>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="BookSearchForm"></FormControl>
+                    <Button variant="outline-info">검색</Button>
+                </Form>
             </Navbar>
         );
     }
