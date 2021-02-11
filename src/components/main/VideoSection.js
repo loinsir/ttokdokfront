f8472ca607899a74d62fd3bf8f17f303f180a70a
loinsir <a9327370@gmail.com>
2021-02-11 14:21:58 +0000

    css 수정

--- src/components/main/VideoSection.js
+++ src/components/main/VideoSection.js
@@ -5,10 +5,12 @@ import ReactPlayer from "react-player";
 class VideoSection extends Component {
     render() {
         return(
-            <Container fluid style={{height:"470px"}}>
-                <Row className="align-items-center">
-                    <Col align="center">
-                        <h1>책 리뷰 영상입니다</h1>
+            <Container fluid>
+                <Row className="align-items-center" style={{height:"30px"}}>
+                    <Col><h1>책 리뷰 영상입니다</h1></Col>
+                </Row>
+                <Row className="align-items-center" style={{height:"460px"}}>
+                    <Col align="center"> 
                         <ReactPlayer url="https://youtu.be/39Bi_INkHfw" controls/>
                     </Col>
                 </Row>
